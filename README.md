9d94259dcec11b90387069c2170464fb4b529330
GabrielRebelles <gabriel.rebelles1@gmail.com>
2020-11-05 18:01:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Online resume
+Online resume: [Visit me](https://www.gabrbl.com/cv/ "Visit me")
---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const path = require("path")
+
+const options = {
+    root: path.join(__dirname),
+    headers: {
+        "Cache-Control": "no-store",
+        "Content-Disposition":`inline; filename="CV Gabriel Rebelles.pdf"`
+    }
+}
+
+app.use((req,res,next)=>{    
+    res.removeHeader('X-Powered-By')
+    res.status(200).sendFile("GabrielRebelles-CV.pdf",options)
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server lanzado en puerto: ${process.env.PORT}`);
+})
